47221b35b187c6a1acb78e65218dcf061a2ade78
Matheus Gomes <matheusgomes192@hotmail.com>
2021-02-02 08:57:14 -0300

    Criando array com itens das linguagens que uso, e já usando

--- src/components/AboutMeComponent.js
+++ src/components/AboutMeComponent.js
@@ -1,12 +1,16 @@
 import {DefaultText} from './DefaultText';
-import Header_img from '../header_img.svg';
+
+import Back from '../svg/back.svg';
+import Mobile from '../svg/mobile.svg';
+import Front from '../svg/front.svg';
 
 import {
     MainDiv,
     TopDiv,
     BlockDiv,
     ItemDiv,
-    Svg
+    Svg,
+    BlockBottomDiv, BlockTopDiv,
 } from './AboutMeStyled';
 
 export default () => {
@@ -14,14 +18,49 @@ export default () => {
     let secondaryColor = '#999';
     let white = '#fff';
 
+    let array = [
+        {
+            id: 1,
+            svg: Front,
+            title: 'Programação Front-end',
+            content: 'Gosto de copiar designs e sempre tentar algo novo, como uma animação, um efeito e etc.',
+            subtitle: 'Linguagens que uso:',
+            languages: ['HTML & CSS'+'\n', 'Bootstrap'+'\n', 'Javascript'+'\n', 'Jquery'+'\n', 'React'+'\n'],
+            bLeft: true,
+            bRight: false,
+        },
+
+        {
+            id: 2,
+            svg: Back,
+            title: 'Programação Back-end',
+            content: 'Com certeza é minha parte favorita da programação, gosto de pensar até resolver o problema de lógica de como fazer tal função.',
+            subtitle: 'Linguagens que uso:',
+            languages: ['PHP'+'\n', 'Laravel'+'\n', 'AJAX'+'\n', 'Firebase'+'\n'],
+            bLeft: false,
+            bRight: false,
+        },
+
+        {
+            id: 3,
+            svg: Mobile,
+            title: 'Programação Mobile',
+            content: 'Essa é a área que tenho menos tempo de estudo, mas é a com mais projetos feitos.',
+            subtitle: 'Linguagens que uso:',
+            languages: ['React-Native'+'\n', 'Firebase'+'\n'],
+            bLeft: false,
+            bRight: true,
+        }
+    ];
+
     return (
         <MainDiv>
             <TopDiv>
-                <DefaultText color={white} align={"center"} bolder={"bold"} font={"22px"}>
+                <DefaultText color={white} align={"center"} bolder={"bold"} font={"25px"}>
                     Meu nome é Matheus, e sou um desenvolvedor Full-Stack em formação
                 </DefaultText>
 
-                <DefaultText width={"60%"} font={"20px"} align={"center"} color={"#ddd"}>
+                <DefaultText width={"75%"} font={"22px"} align={"center"} color={"#ddd"}>
                     Oi, tenho 19 anos e estou na área de programação à 2 anos. Comecei quando iniciei a faculdade de
                     Análise
                     e Desenvolvimento de Sistemas, a qual estou no 5º semestre. Não tenho nenhuma experiência
@@ -31,36 +70,28 @@ export default () => {
             </TopDiv>
 
             <BlockDiv>
-                <ItemDiv bLeft={"1px solid #999"} radius={"10px 0 0 10px"}>
-                    <Svg src={Header_img} width={100} height={100} fill={"#845EC2"}/>
-                    <DefaultText color={primaryColor} align={"center"} bolder={"bold"}>Programação Front-end</DefaultText>
+                {array.map((item, k) => (
+                    <ItemDiv key={k} bLeft={item.bLeft ? '1px solid #999' : null} rightTop={item.bRight ? '10px' : null} rightBottom={item.bRight ? '10px' : null} leftTop={item.bLeft ? '10px' : null} leftBottom={item.bLeft ? '10px' : null}>
+                        <BlockTopDiv>
+                            <Svg src={item.svg} width={80} height={80} fill={"#845EC2"}/>
 
-                    <DefaultText color={secondaryColor} align={"center"}>
-                        Gosto de copiar designs e sempre tentar algo novo, como uma animação, um efeito e etc.
-                    </DefaultText>
+                            <DefaultText color={primaryColor} font={"18px"} align={"center"} bolder={"bold"}>{item.title}</DefaultText>
 
-                    <DefaultText align={"center"} color={"#845EC2"} bolder={"bold"} mTop={"80px"}>Linguagens que uso:</DefaultText>
-                    <DefaultText align={"center"} bolder={"bold"} color={primaryColor} mTop={"20px"}>HTML & CSS</DefaultText>
-                </ItemDiv>
+                            <DefaultText style={{height: 120}} font={"18px"} color={secondaryColor} align={"center"}> {item.content} </DefaultText>
+                        </BlockTopDiv>
 
-                <ItemDiv>
-                    <Svg src={Header_img} width={100} height={100} fill={"#845EC2"}/>
-                    <DefaultText color={primaryColor} align={"center"} bolder={"bold"}>Programação Front-end</DefaultText>
+                        <BlockBottomDiv>
+                            <DefaultText font={"18px"} align={"center"} color={"#845EC2"} bolder={"bold"}>
+                                {item.subtitle}
+                            </DefaultText>
 
-                    <DefaultText color={secondaryColor} align={"center"}>
-                        Gosto de copiar designs e sempre tentar algo novo, como uma animação, um efeito e etc.
-                    </DefaultText>
+                            <DefaultText font={"18px"} style={{whiteSpace: 'pre-wrap'}} align={"center"} bolder={"bold"} color={primaryColor}>
+                                {item.languages}
+                            </DefaultText>
+                        </BlockBottomDiv>
 
-
-                </ItemDiv>
-
-                <ItemDiv radius={"0 10px 10px 0"}>
-                    <Svg src={Header_img} width={100} height={100} fill={"#845EC2"}/>
-                    <DefaultText color={primaryColor} align={"center"} bolder={"bold"}>Programação Front-end</DefaultText>
-                    <DefaultText color={secondaryColor} align={"center"}>
-                        Gosto de copiar designs e sempre tentar algo novo, como uma animação, um efeito e etc.
-                    </DefaultText>
-                </ItemDiv>
+                    </ItemDiv>
+                ))}
             </BlockDiv>
         </MainDiv>
     );
--- src/components/AboutMeStyled.js
+++ src/components/AboutMeStyled.js
@@ -29,10 +29,26 @@ export const ItemDiv = styled.div`
     padding: 20px;
     background-color: #fff;
     height: 600px;
-    width: 20%;
-    border-radius: ${props=>props.radius};
+    width: 25%;
     border-right: 1px solid #999;
     border-left: ${props=>props.bLeft};
+    border-top-left-radius: ${props=>props.leftTop};
+    border-bottom-left-radius: ${props=>props.leftBottom};
+    border-top-right-radius: ${props=>props.rightTop};
+    border-bottom-right-radius: ${props=>props.rightBottom};
     border-bottom: 1px solid #999;
 `;
 export const Svg = styled.img``;
+export const BlockTopDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 20px;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 0;
+`;
+export const BlockBottomDiv = styled.div`
+    display: flex;
+    flex-direction: column; 
+    margin-top: 80px;
+`;
