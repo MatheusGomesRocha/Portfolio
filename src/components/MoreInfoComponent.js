797d15ca022642ec2b95bc13839577c380dc5576
Matheus Gomes <matheusgomes192@hotmail.com>
2021-02-03 08:24:44 -0300

    Adicionando novo SVG Coffee e mudando cores para roxo de todos + fazendo div em grid com efeito de aumentar os números

--- src/components/MoreInfoComponent.js
+++ src/components/MoreInfoComponent.js
@@ -3,67 +3,78 @@ import AnimatedNumber from "react-animated-number";
 import Smile from '../svg/smile.svg';
 import Clock from '../svg/clock.svg';
 import Edit from '../svg/edit.svg';
+import Coffee from '../svg/coffee-cup.svg';
 
 import {
     MainDiv,
     BlockDiv,
     Svg
 } from "./MoreInfoStyled";
+import {useState} from "react";
 
 export default () => {
+    const [hover1, setHover1] = useState(false);
+    const [hover2, setHover2] = useState(false);
+    const [hover3, setHover3] = useState(false);
+    const [hover4, setHover4] = useState(false);
+
     return (
         <MainDiv>
 
-            <BlockDiv>
-                <Svg src={Smile}/>
-                <AnimatedNumber component="text" value={174}
+            <BlockDiv onMouseOver={() => setHover1(true)} onMouseOut={() => setHover1(false)} background={"#eee"}>
+                <Svg transform={hover1 ? 'scale(1.2)' : 'scale(1)'} src={Smile}/>
+                <AnimatedNumber component="text" value={18}
                                 style={{
                                     fontSize: 48,
-
+                                    color: '#845EC2',
+                                    marginTop: 10
                                 }}
                                 formatValue={n => n.toFixed(0)}
                                 duration={3000}
                 />
-                <DefaultText>Clientes satisfeitos</DefaultText>
+                <DefaultText align={"center"} bolder={"bold"} font={"25px"} mTop={"10px"}>Clientes satisfeitos</DefaultText>
             </BlockDiv>
 
-            <BlockDiv>
-                <Svg src={Smile}/>
-                <AnimatedNumber component="text" value={1.478}
+            <BlockDiv onMouseOver={() => setHover2(true)} onMouseOut={() => setHover2(false)}>
+                <Svg transform={hover2 ? 'scale(1.2)' : 'scale(1)'} src={Coffee}/>
+                <AnimatedNumber component="text" value={1478}
                                 style={{
                                     fontSize: 48,
-
+                                    color: '#845EC2',
+                                    marginTop: 10
                                 }}
                                 formatValue={n => n.toFixed(0)}
                                 duration={3000}
                 />
-                <DefaultText>Copos de café</DefaultText>
+                <DefaultText align={"center"} bolder={"bold"} font={"25px"} mTop={"10px"}>Copos de café</DefaultText>
             </BlockDiv>
 
-            <BlockDiv>
-                <Svg src={Edit}/>
-                <AnimatedNumber component="text" value={1.871}
+            <BlockDiv onMouseOver={() => setHover3(true)} onMouseOut={() => setHover3(false)} background={"#eee"}>
+                <Svg transform={hover3 ? 'scale(1.2)' : 'scale(1)'} src={Edit}/>
+                <AnimatedNumber component="text" value={1871}
                                 style={{
                                     fontSize: 48,
-
+                                    color: '#845EC2',
+                                    marginTop: 10
                                 }}
                                 formatValue={n => n.toFixed(0)}
                                 duration={3000}
                 />
-                <DefaultText>Linhas de códigos</DefaultText>
+                <DefaultText align={"center"} bolder={"bold"} font={"25px"} mTop={"10px"}>Linhas de códigos</DefaultText>
             </BlockDiv>
 
-            <BlockDiv>
-                <Svg src={Clock}/>
+            <BlockDiv onMouseOver={() => setHover4(true)} onMouseOut={() => setHover4(false)}>
+                <Svg transform={hover4 ? 'scale(1.2)' : 'scale(1)'} src={Clock}/>
                 <AnimatedNumber component="text" value={5}
                                 style={{
                                     fontSize: 48,
-
+                                    color: '#845EC2',
+                                    marginTop: 10
                                 }}
                                 formatValue={n => n.toFixed(0)}
                                 duration={3000}
                 />
-                <DefaultText>Projetos completados</DefaultText>
+                <DefaultText align={"center"} bolder={"bold"} font={"25px"} mTop={"10px"}>Projetos completados</DefaultText>
             </BlockDiv>
         </MainDiv>
     )
--- src/components/MoreInfoStyled.js
+++ src/components/MoreInfoStyled.js
@@ -1,5 +1,25 @@
 import styled from 'styled-components';
 
-export const MainDiv = styled.div``;
-export const BlockDiv = styled.div``;
-export const Svg = styled.img``;
+export const MainDiv = styled.div`
+    display: grid;
+    grid-template-columns: 25% 25% 25% 25%;
+    margin-top: 600px;
+`;
+export const BlockDiv = styled.div`
+    background-color: ${props=>props.background};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 40px 0;
+    
+    :hover {
+        cursor: pointer;
+    }
+`;
+export const Svg = styled.img`
+    width: 80px;
+    height: 80px;
+    transition: 0.5s;
+    
+    transform: ${props=>props.transform};
+`;
