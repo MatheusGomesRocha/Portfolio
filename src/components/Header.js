e1cab20506d1654cd1635b854bd54e9aa6c4c28d
Matheus Gomes <matheusgomes192@hotmail.com>
2021-04-06 10:30:11 -0300

    Criando 2 novos diretóries (assets para imgs e svgs, sideComponent para components que vou reutilizar) + criando DefaultBtn no sideComponent + começando a estilizar o headerBottom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from "react";
 import './Header.css';
 
+import DefaultBtnComponent from "../sideComponent/DefaultBtn";
+import HeaderSvg from '../assets/svg/header';
+
 export default () => {
     const [fixedHeaderTop, setFixedHeaderTop] = useState(false);
     const [link, setLink] = useState('home');
@@ -20,7 +23,7 @@ export default () => {
         };
     }, [])
 
-    return(
+    return (
         <div className={"header"}>
             <div className={"headerTop"} style={{
                 boxShadow: fixedHeaderTop && '0 1px 5px 0 rgba(0, 0, 0, 0.75)',
@@ -35,22 +38,26 @@ export default () => {
                     </div>
 
                     <div onClick={() => setLink('about')} className={"linkDiv"}>
-                        <a className={"link"} href={"#about"} style={{color: link === 'about' && '#FEB633'}}>About Me</a>
+                        <a className={"link"} href={"#about"} style={{color: link === 'about' && '#FEB633'}}>About
+                            Me</a>
                         <div className={"linkBorderBottom"} style={{width: link === 'about' && '30px'}}></div>
                     </div>
 
                     <div onClick={() => setLink('services')} className={"linkDiv"}>
-                        <a className={"link"} href={"#services"} style={{color: link === 'services' && '#FEB633'}}>Services</a>
+                        <a className={"link"} href={"#services"}
+                           style={{color: link === 'services' && '#FEB633'}}>Services</a>
                         <div className={"linkBorderBottom"} style={{width: link === 'services' && '30px'}}></div>
                     </div>
 
                     <div onClick={() => setLink('skills')} className={"linkDiv"}>
-                        <a className={"link"} href={"#skills"} style={{color: link === 'skills' && '#FEB633'}}>Skills</a>
+                        <a className={"link"} href={"#skills"}
+                           style={{color: link === 'skills' && '#FEB633'}}>Skills</a>
                         <div className={"linkBorderBottom"} style={{width: link === 'skills' && '30px'}}></div>
                     </div>
 
                     <div onClick={() => setLink('process')} className={"linkDiv"}>
-                        <a className={"link"} href={"#process"} style={{color: link === 'process' && '#FEB633'}}>Process</a>
+                        <a className={"link"} href={"#process"}
+                           style={{color: link === 'process' && '#FEB633'}}>Process</a>
                         <div className={"linkBorderBottom"} style={{width: link === 'process' && '30px'}}></div>
                     </div>
 
@@ -60,44 +67,24 @@ export default () => {
                     </div>
 
                     <div onClick={() => setLink('contact')} className={"linkDiv"}>
-                        <a className={"link"} href={"#contact"} style={{color: link === 'contact' && '#FEB633'}}>Contact</a>
+                        <a className={"link"} href={"#contact"}
+                           style={{color: link === 'contact' && '#FEB633'}}>Contact</a>
                         <div className={"linkBorderBottom"} style={{width: link === 'contact' && '30px'}}></div>
                     </div>
                 </div>
             </div>
 
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
-            <p>a</p>
+            <div className={"headerBottom"}>
+                <div className={"side"}>
+                    <span className={"title"}>Meu nome é Matheus</span>
+                    <span className={"text"}>Programador Web & Mobile</span>
+                    <DefaultBtnComponent title={"Hire Me"}/>
+                </div>
 
+                <div className={"side"}>
+                    <HeaderSvg />
+                </div>
+            </div>
 
         </div>
     )
---
+++ src/sideComponent/DefaultBtn.js
@@ -0,0 +1,31 @@
+
+import {withStyles} from "@material-ui/core/styles";
+import {Button} from "@material-ui/core";
+
+export default(props) => {
+    const DefaultBtn = withStyles(() => ({
+        root: {
+            padding: 20,
+            height: props.height,
+            marginTop: props.mTop,
+            borderRadius: 5,
+            width: props.width,
+            backgroundColor: '#CD553F',
+            color: '#fff',
+            fontWeight: 'bold',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            transition: '500ms',
+            textTransform: props.transform ? props.transform : 'uppercase',
+
+            '&:hover': {
+                backgroundColor: '#000',
+            }
+        },
+    }))(Button);
+
+    return (
+        <DefaultBtn>{props.title}</DefaultBtn>
+    )
+}
