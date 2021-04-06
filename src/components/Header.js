3314f061f9fdf9288f937f48556b6c2a36935963
Matheus Gomes <matheusgomes192@hotmail.com>
2021-04-06 10:18:20 -0300

    Finalizando HeaderTop, com efeito de aparecer borderBottom no link ao passar mouse em cima e mudando a cordo link dependendo de que link o usuário selecionou

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import './Header.css';
 
 export default () => {
     const [fixedHeaderTop, setFixedHeaderTop] = useState(false);
+    const [link, setLink] = useState('home');
 
     const handleScroll = () => {        // Verifica o Scroll da pagina
         if (document.documentElement.scrollTop > 90) {       // Se for maior que 0, fixa o Header e está na sessão Home
@@ -22,19 +23,46 @@ export default () => {
     return(
         <div className={"header"}>
             <div className={"headerTop"} style={{
-                borderBottom: fixedHeaderTop && '2px solid red',
+                boxShadow: fixedHeaderTop && '0 1px 5px 0 rgba(0, 0, 0, 0.75)',
                 backgroundColor: fixedHeaderTop && '#252525'
             }}>
-                <span className={"Logo"}>M.</span>
+                <span className={"logo"}>M.</span>
 
                 <div className={"side"}>
-                    <a className={"link"} href={"#home"}>Home</a>
-                    <a className={"link"} href={"#about"}>About Me</a>
-                    <a className={"link"} href={"#services"}>Services</a>
-                    <a className={"link"} href={"#skills"}>Skills</a>
-                    <a className={"link"} href={"#process"}>Working Process</a>
-                    <a className={"link"} href={"#works"}>Works</a>
-                    <a className={"link"} href={"#contact"}>Contact</a>
+                    <div onClick={() => setLink('home')} className={"linkDiv"}>
+                        <a className={"link"} href={"#home"} style={{color: link === 'home' && '#FEB633'}}>Home</a>
+                        <div className={"linkBorderBottom"} style={{width: link === 'home' && '30px'}}></div>
+                    </div>
+
+                    <div onClick={() => setLink('about')} className={"linkDiv"}>
+                        <a className={"link"} href={"#about"} style={{color: link === 'about' && '#FEB633'}}>About Me</a>
+                        <div className={"linkBorderBottom"} style={{width: link === 'about' && '30px'}}></div>
+                    </div>
+
+                    <div onClick={() => setLink('services')} className={"linkDiv"}>
+                        <a className={"link"} href={"#services"} style={{color: link === 'services' && '#FEB633'}}>Services</a>
+                        <div className={"linkBorderBottom"} style={{width: link === 'services' && '30px'}}></div>
+                    </div>
+
+                    <div onClick={() => setLink('skills')} className={"linkDiv"}>
+                        <a className={"link"} href={"#skills"} style={{color: link === 'skills' && '#FEB633'}}>Skills</a>
+                        <div className={"linkBorderBottom"} style={{width: link === 'skills' && '30px'}}></div>
+                    </div>
+
+                    <div onClick={() => setLink('process')} className={"linkDiv"}>
+                        <a className={"link"} href={"#process"} style={{color: link === 'process' && '#FEB633'}}>Process</a>
+                        <div className={"linkBorderBottom"} style={{width: link === 'process' && '30px'}}></div>
+                    </div>
+
+                    <div onClick={() => setLink('works')} className={"linkDiv"}>
+                        <a className={"link"} href={"#works"} style={{color: link === 'works' && '#FEB633'}}>Works</a>
+                        <div className={"linkBorderBottom"} style={{width: link === 'works' && '30px'}}></div>
+                    </div>
+
+                    <div onClick={() => setLink('contact')} className={"linkDiv"}>
+                        <a className={"link"} href={"#contact"} style={{color: link === 'contact' && '#FEB633'}}>Contact</a>
+                        <div className={"linkBorderBottom"} style={{width: link === 'contact' && '30px'}}></div>
+                    </div>
                 </div>
             </div>
 
